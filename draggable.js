82f9773499ccb3ced5039c7736479aec26a32916
adellhk <adell.hk@gmail.com>
2015-02-13 15:07:22 -0800

    adds more droppable functionoality. Can now alert or console log the column number

--- draggable.js
+++ draggable.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
 
-  $('#redStack').draggable({
+  $('#redStack, #blueStack').draggable({
     containment: 'document',
-    cursore: 'move',
+    cursor: 'move',
     snap: '.mouth'
 
   });
 
+  $('.mouth').droppable({
+    hoverClass: 'mouth-hover',
+    drop: function (e) {
+      var column = e.target.innerText;
+      console.log('Dropped into column # '+ column;
+      // board.place(column)
+    }
+  })
 
 
+  // $('mouth').on("dropover", function(e){
+  // });
 
 })
